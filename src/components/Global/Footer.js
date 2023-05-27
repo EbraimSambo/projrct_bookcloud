a56f71af31c9ee7b8bb717410d36d60fb7dc6157
Ebraim Da Costa Sambo <107747841+EbraimSambo@users.noreply.github.com>
2023-05-27 22:20:01 +0000

    footer start

--- src/components/Global/Footer.js
+++ src/components/Global/Footer.js
@@ -1,9 +1,28 @@
+import Link from 'next/link'
 import React from 'react'
-
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { SiWattpad, SiGmail} from "react-icons/si";
 const Footer = () => {
   return (
         <footer className='footer'>
-            <p> 2023 &copy; Tutorial </p>
+          
+          <ul className='maplinks'>
+            <li> <Link href={"/about"}>Sobre Nos</Link> </li>
+            <li> <Link href={"/about"}>Termos</Link> </li>
+            <li> <Link href={"/about"}>Privacidade</Link> </li>
+            <li> <Link href={"/about"}>Politicas</Link> </li>
+            <li> <Link href={"/about"}>Acessibilidade</Link> </li>
+            <li> <Link href={"/about"}>Ajuda</Link> </li>
+            <li> 2023 &copy; Book Cloud</li>
+          </ul>
+      
+          <ul className='midiasocial'>
+            <li> <Link href={"/about"} > <FaFacebook /> </Link> </li>
+            <li> <Link href={"/about"}> <FaInstagram /> </Link> </li>
+            <li> <Link href={"/about"}> <SiGmail /> </Link> </li>
+            <li> <Link href={"/about"}> <SiWattpad /> </Link> </li>
+          </ul>
+
         </footer>
   )
 }
