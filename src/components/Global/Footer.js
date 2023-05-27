ade2c23fe8d5af76d80b30ddc0066d228601e579
Ebraim Da Costa Sambo <107747841+EbraimSambo@users.noreply.github.com>
2023-05-27 22:55:20 +0000

    finish footer

--- src/components/Global/Footer.js
+++ src/components/Global/Footer.js
@@ -1,18 +1,19 @@
 import Link from 'next/link'
 import React from 'react'
-import { FaFacebook, FaInstagram } from "react-icons/fa";
-import { SiWattpad, SiGmail} from "react-icons/si";
+import { FaFacebook, FaInstagram, FaWindows, FaLinux } from "react-icons/fa";
+import { SiWattpad, SiGmail, SiAndroid, SiIos} from "react-icons/si";
 const Footer = () => {
   return (
         <footer className='footer'>
-          
+
           <ul className='maplinks'>
             <li> <Link href={"/about"}>Sobre Nos</Link> </li>
-            <li> <Link href={"/about"}>Termos</Link> </li>
-            <li> <Link href={"/about"}>Privacidade</Link> </li>
-            <li> <Link href={"/about"}>Politicas</Link> </li>
-            <li> <Link href={"/about"}>Acessibilidade</Link> </li>
-            <li> <Link href={"/about"}>Ajuda</Link> </li>
+            <li> <Link href={"/apps"}>Aplicativos</Link> </li>
+            <li> <Link href={"/terms"}>Termos</Link> </li>
+            <li> <Link href={"/privacy"}>Privacidade</Link> </li>
+            <li> <Link href={"/politcs"}>Politicas</Link> </li>
+            <li> <Link href={"/acessiblity"}>Acessibilidade</Link> </li>
+            <li> <Link href={"/help"}>Ajuda</Link> </li>
             <li> 2023 &copy; Book Cloud</li>
           </ul>
       
@@ -20,7 +21,12 @@ const Footer = () => {
             <li> <Link href={"/about"} > <FaFacebook /> </Link> </li>
             <li> <Link href={"/about"}> <FaInstagram /> </Link> </li>
             <li> <Link href={"/about"}> <SiGmail /> </Link> </li>
-            <li> <Link href={"/about"}> <SiWattpad /> </Link> </li>
+            <li> <Link href={"/about"}> <SiWattpad /> </Link> </li>            
+            <li> . </li>
+            <li> <Link href={"/about"}> <SiAndroid /> </Link> </li>
+            <li> <Link href={"/about"}> <FaWindows /> </Link> </li>
+            <li> <Link href={"/about"}> <FaLinux /> </Link> </li>
+            <li> <Link href={"/about"}> <SiIos /> </Link> </li>
           </ul>
 
         </footer>
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '@/components/Layout/Layout'
+import React from 'react'
+
+const about = () => {
+  return (
+    <Layout title={"Sobre"}>
+        <h2>About</h2>
+    </Layout>
+  )
+}
+
+export default about
